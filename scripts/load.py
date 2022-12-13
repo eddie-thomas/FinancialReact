2229775937fa7eec00f2d394a5a468fd66990410
Eddie <eddie.thomas@semanticarts.com>
2022-12-13 10:20:03 -0700

    Update the python file to correctly match on the rows

--- scripts/load.py
+++ scripts/load.py
@@ -1,26 +1,27 @@
 from py_pdf_parser.loaders import load_file
 from py_pdf_parser.visualise import visualise
 
-def get_page():
-    return load_file("../pdfs-sensative/Document.pdf")
+import re
+
+def get_page(): #
+    return load_file("../pdfs-sensative/Document.pdf", { "line_overlap":0.01, "line_margin":0.01 })
 
 if "__main__" == __name__:
     doc = get_page()
-    date = doc.pages[0].elements.filter_by_regex(r"[1]?[0-9]\/[1-3]?[0-9]")[2]
-    print(date.text())
-    expected = doc.pages[0].elements.horizontally_in_line_with(date)
-    # doc.pages[0].elements.filter_by_fonts("Helvetica,7.8",  "Helvetica-Bold,7.8").__or__(doc.pages[1].elements.filter_by_fonts("Helvetica,7.8",  "Helvetica-Bold,7.8"))
-    visualise(doc, elements=expected)
-    # doc = get_page()
-    # for element in doc.pages[1].elements:
-    #     print(element.font)
+    accounting_number = doc.elements.to_the_right_of(doc.elements.filter_by_regex(r"^account number$", re.IGNORECASE)[0])
+    billing_period = doc.elements.to_the_right_of(doc.elements.filter_by_regex(r"^statement billing period$", re.IGNORECASE)[0])
+    # visualise(doc, elements=billing_period|accounting_number)
+    print(accounting_number.extract_single_element().text())
+    print(billing_period.extract_single_element().text())
+    dates = doc.elements.filter_by_regex(r"^[1|0]?[0-9]\/[0-3]?[0-9]$")
+
+    for date in dates:
+        rowelements = doc.elements.horizontally_in_line_with(date).add_tag_to_elements("test")
+
+    lines = doc.elements.filter_by_tag("test")
+    visualise(doc, elements=lines)
+
 
 # @see {https://pdfminersix.readthedocs.io/en/latest/reference/composable.html#laparams}
 
 
-    #
-    # for element in expected:
-    #     print(element.text())
-    # print("########## table ##########")
-    # table = extract_table(expected, True, True, True, True)
-    # print(table)
