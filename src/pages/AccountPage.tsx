2882b9b2a9d4d5fe3fd00c11180c0b2449ea29e2
Eddie <eddie.thomas@semanticarts.com>
2023-01-01 19:51:39 -0700

    Complete the "my account" page

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -1,9 +1,25 @@
 // interface Props {}
 
 import { memo } from "react";
+import { Typography } from "@mui/material";
+
+import { StyledPaper } from "../components/TransactionTable";
+import { useContext } from "../utils/Context";
 
 function AccountPage() {
-  return <>My Account</>;
+  const app = useContext();
+  return (
+    <StyledPaper
+      sx={{ height: "100%", "& > *": { margin: (theme) => theme.spacing(5) } }}
+    >
+      <Typography sx={{ display: "flex", justifyContent: "space-around" }}>
+        Name:<b>{app.user?.name}</b>
+      </Typography>
+      <Typography sx={{ display: "flex", justifyContent: "space-around" }}>
+        Accounts:<b>{app.user?.accounts.join(", ")}</b>
+      </Typography>
+    </StyledPaper>
+  );
 }
 
 export default memo(AccountPage);
